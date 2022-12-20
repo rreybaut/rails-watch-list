c69591ec90e371b5a511e2a9611cdb0038f311be
raphael reybaut <rreybaut@gmail.com>
2022-12-20 12:39:37 +0100

    models ok

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+  # has_one_attached :photo # Ajout
+  validates :name, uniqueness: true, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_132818) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_104818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,8 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_132818) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
-    t.string "overview"
-    t.string "poster_url"
+    t.text "overview"
+    t.text "poster_url"
     t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
